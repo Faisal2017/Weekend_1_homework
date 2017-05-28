2bd479c0e8f2612e23498c33e1f0e28a22f406c1
Faisal <faisal_a@live.co.uk>
2017-05-28 19:05:43 +0100

    Adding cash to pet shop function defined

--- pet_shop.rb
+++ pet_shop.rb
@@ -3,5 +3,9 @@ def pet_shop_name(pet_shop)
 end  
 
 def total_cash(pet_shop)
-  @pet_shop[:admin][:total_cash]
+  return @pet_shop[:admin][:total_cash]
 end  
+
+def add_or_remove_cash(pet_shop, customer_cash)
+  return @pet_shop[:admin][:total_cash] += customer_cash
+end
