1a979a42de9799276e5d21385112c28d019f5c7d
Faisal <faisal_a@live.co.uk>
2017-05-28 20:04:09 +0100

    Adding & removing cash function fully defined

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,6 +6,8 @@ def total_cash(pet_shop)
   return @pet_shop[:admin][:total_cash]
 end  
 
-def add_or_remove_cash(pet_shop, customer_cash)
-  return @pet_shop[:admin][:total_cash] += customer_cash
-end
+def add_or_remove_cash(pet_shop, customer_cash)  
+  return @pet_shop[:admin][:total_cash] += customer_cash if (customer_cash > 0)
+  return @pet_shop[:admin][:total_cash] += customer_cash if (customer_cash < 0) 
+end  
+
